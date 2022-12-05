1efce8c6abea855c2f6563e6c0da0eb941ddbac0
Kongnapat Jirawatangkoon <thelaval.lol@gmail.com>
2022-12-05 18:55:25 +0700

    create/edit staff

--- pages/management/staff/_staff_id.vue
+++ pages/management/staff/_staff_id.vue
@@ -8,26 +8,40 @@
             <div class="mt-8" style="background-color: grey; width: 200px; height: 200px; border-radius: 50%;"/>
           </div>
           <v-col>
-            <v-text-field v-model="form.code" name="code" label="รหัสพนักงาน*" required/>
-            <v-text-field v-model="form.fistNameTh" label="ชื่อ*" required/>
-            <v-text-field v-model="form.fistNameEn" label="ชื่อ (Eng)"/>
-            <v-select v-model="form.division" :items="items" itemValue="id" itemText="name" label="กอง"/>
-            <v-text-field v-model="form.username" label="รหัสผู้ใช้งาน"/>
-            <v-text-field v-model="form.password" label="รหัสผ่าน" :type="seePassword ? 'text' : 'password'">
+            <v-text-field v-model="form.code" name="code" label="รหัสพนักงาน *" :rules="codeRules" required/>
+            <v-text-field v-model="form.fistNameTh" label="ชื่อ *" :rules="fistNameThRules" required/>
+            <v-text-field v-model="form.fistNameEn" label="ชื่อ (Eng) *" :rules="fistNameEnRules" required/>
+            <v-select v-model="form.division" :items="items" itemValue="id" itemText="name" label="กอง *" :rules="divisionRules" required/>
+            <v-text-field v-model="form.username" label="รหัสผู้ใช้งาน *" :rules="usernameRules" required/>
+            <v-text-field v-model="form.password" label="รหัสผ่าน *" :type="seePassword ? 'text' : 'password'" :rules="passwordRules" required>
               <template #append><v-icon @click="seePassword = !seePassword" v-text="`mdi-eye${seePassword ? '-off' : ''}`"/></template>
             </v-text-field>
           </v-col>
           <v-col>
-            <v-text-field v-model="form.idCard" label="รหัสบัตรประชาชน*"/>
-            <v-text-field v-model="form.lastNameTh" label="นามสกุล"/>
-            <v-text-field v-model="form.lastNameEn" label="นามสกุล (Eng)"/>
-            <v-select v-model="form.group" :items="items" itemValue="id" itemText="name" label="กลุ่ม"/>
-            <v-select v-model="form.position" :items="items" itemValue="id" itemText="name" label="ตำแหน่ง"/>
-            <v-select v-model="form.role" :items="items" itemValue="id" itemText="name" label="สิทธ์การใช้งาน"/>
+            <v-text-field v-model="form.idCard" label="รหัสบัตรประชาชน *" :rules="idCardRules" required/>
+            <v-text-field v-model="form.lastNameTh" label="นามสกุล *" :rules="lastNameThRules" required/>
+            <v-text-field v-model="form.lastNameEn" label="นามสกุล (Eng) *" :rules="lastNameEnRules" required/>
+            <v-select v-model="form.group" :items="items" itemValue="id" itemText="name" label="กลุ่ม *" :rules="groupRules" required/>
+            <v-select v-model="form.position" :items="items" itemValue="id" itemText="name" label="ตำแหน่ง *" :rules="positionRules" required/>
+            <v-select v-model="form.role" :items="items" itemValue="id" itemText="name" label="สิทธ์การใช้งาน *" :rules="roleRules" required/>
           </v-col>
         </v-row>
-        <v-row class="mt-12" justify="end">
-          <v-btn elevation="2" large color="success">บันทึก</v-btn>
+      </v-container>
+      <v-container>
+        <h3 class="text-xl font-bold mb-2">ข้อมูลติดต่อ</h3>
+        <v-row>
+          <v-col>
+            <v-text-field v-model="form.tel" name="tel" label="เบอร์โทรศัพท์"/>
+          </v-col>
+          <v-col>
+            <v-text-field v-model="form.email" name="email" label="E-Mail"/>
+          </v-col>
+        </v-row>
+        <v-textarea v-model="form.address" name="address" label="ที่อยู่"/>
+      </v-container>
+      <v-container class="mt-6">
+        <v-row justify="end">
+          <v-btn elevation="2" large color="success" @click="onSave">บันทึก</v-btn>
         </v-row>
       </v-container>
     </v-form>
@@ -54,7 +68,10 @@
           division: null,
           group: null,
           position: null,
-          role: null
+          role: null,
+          tel: '',
+          email: '',
+          address: ''
         },
         items: [
           { id: 1, name: 'Foo' },
@@ -62,12 +79,53 @@
           { id: 3, name: 'Fizz' },
           { id: 4, name: 'Buzz' }
         ],
-        seePassword: false
+        seePassword: false,
+        codeRules: [
+          v => !!v || 'โปรดใส่รหัสพนักงาน',
+        ],
+        idCardRules: [
+          v => !!v || 'โปรดใส่รหัสบัตรประชาชน',
+        ],
+        fistNameThRules: [
+          v => !!v || 'โปรดใส่ชื่อ',
+        ],
+        lastNameThRules: [
+          v => !!v || 'โปรดใส่นามสกุล',
+        ],
+        fistNameEnRules: [
+          v => !!v || 'โปรดใส่ชื่อ (Eng)',
+        ],
+        lastNameEnRules: [
+          v => !!v || 'โปรดใส่นามสกุล (Eng)',
+        ],
+        usernameRules: [
+          v => !!v || 'โปรดใส่รหัสผู้ใช้งาน',
+        ],
+        passwordRules: [
+          v => !!v || 'โปรดใส่รหัสผ่าน',
+        ],
+        divisionRules: [
+          v => !!v || 'โปรดใส่กอง',
+        ],
+        groupRules: [
+          v => !!v || 'โปรดใส่กลุ่ม',
+        ],
+        positionRules: [
+          v => !!v || 'โปรดใส่ตำแหน่ง',
+        ],
+        roleRules: [
+          v => !!v || 'โปรดใส่สิทธ์การใช้งาน',
+        ],
       }
     },
     computed: {
       isCreate () {
         return this.$route.params.staff_id === 'create'
+      },
+    },
+    methods: {
+      onSave () {
+        this.$refs.form.validate()
       }
     }
   }
