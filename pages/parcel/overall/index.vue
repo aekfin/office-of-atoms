f8364dab6a8051b80e5a837e4fa3f0d3971b9970
Kongnapat Jirawatangkoon <thelaval.lol@gmail.com>
2023-06-11 19:12:18 +0700

    parcel report

--- pages/parcel/overall/index.vue
+++ pages/parcel/overall/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="parcel-page">
     <PageHeader text="บริหารวัสดุคงคลัง" btnText="เพิ่มวัสดุคงคลัง" createRoute="/parcel/overall/create/" :total="total"/>
+    <ExportReportButton apiPath="report/pacelAll" name="รายงานวัสดุคงคลัง"/>
     <v-data-table :headers="headers" :items="items" :itemsPerPage="20" disableSort hideDefaultFooter class="elevation-1 mt-6" :loading="isLoading">
       <template #item.order="{ index }">{{ $store.state.paginationIndex + index + 1 }}</template>
       <template #item.dateEntry="{ item }">
@@ -18,6 +19,7 @@
   export default {
     components: {
       PageHeader: () => import('~/components/PageHeader.vue'),
+      ExportReportButton: () => import('~/components/ExportReportButton.vue'),
       Pagination: () => import('~/components/Pagination.vue'),
     },
     data () {
