b812c321703e36b1cf9569c3d8e7b05a67c0eccf
Aek Kongnapat <thelaval.lol@gmail.com>
2023-09-15 09:37:42 +0700

    add new report

--- pages/report/index.vue
+++ pages/report/index.vue
@@ -44,6 +44,12 @@
           <ExportReportButton apiPath="report/project" name="รายงานการรอคืนหลักประกัน"/>
         </v-col>
       </v-row>
+      <v-row class="mt-10">
+        <v-col :col="12" :md="4">
+          <h4 class="text-xl mb-2"><b>รายงานสรุปผลจัดซื้อจัดจ้าง</b></h4>
+          <ExportReportButton apiPath="report/procurementSummary" name="รายงานสรุปผลจัดซื้อจัดจ้าง"/>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
